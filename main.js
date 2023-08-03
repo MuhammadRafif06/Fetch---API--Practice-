6030af7aef6365276d3ac1275a4101e091946993
Muhammad Rafif <radif.adityo6@gmail.com>
2023-08-03 13:40:22 +0700

    Updated (2)

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const volcanoTableBody = document.querySelector('#volcanoTable tbody');
-
     axios.get('https://indonesia-public-static-api.vercel.app/api/volcanoes')
         .then(response => {
+
             const volcanoes = response.data;
             volcanoes.forEach(volcano => {
+                
                 const row = document.createElement('tr');
                 row.innerHTML = `
-      <td>${volcano.nama}</td>
-      <td>${volcano.bentuk}</td>
-      <td>${volcano.tinggi_meter}</td>
-      <td>${volcano.estimasi_letusan_terakhir}</td>
-      <td>${volcano.geolokasi}</td>
-    `;
+                    <td>${volcano.nama}</td>
+                    <td>${volcano.bentuk}</td>
+                    <td>${volcano.tinggi_meter}</td>
+                    <td>${volcano.estimasi_letusan_terakhir}</td>
+                    <td>${volcano.geolokasi}</td>`;
                 volcanoTableBody.appendChild(row);
+
             });
         })
 });
